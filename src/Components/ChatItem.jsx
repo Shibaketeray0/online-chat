8c96cff99cbc9b06c66ad4e284c0ac7b6d543b13
Denys <denishomov2015@gmail.com>
2023-08-17 18:00:07 +0300

    Changes

--- src/Components/ChatItem.jsx
+++ src/Components/ChatItem.jsx
@@ -3,7 +3,7 @@ export default function ChatItem({item}) {
         <div className="chat-container">
             <a href="#" className={"open-chat-button"}>
                 <div className="status">
-                    <div className="avatar"></div>
+                    <div className="avatar">{item.Picture}</div>
                     <div className="online-badge"></div>
                 </div>
                 <div className="info">
--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,11 +1,35 @@
 import Toolbar from "./Toolbar.jsx";
+import ChatItem from "./ChatItem.jsx";
+import {db} from "../firebase.js";
+import {useState, useEffect} from "react";
+import {collection, getDocs} from "firebase/firestore";
+
 export default function ChatList() {
+    const [users, setUsers] = useState([]);
+    (async () => {
+        let usr_list = [];
+        const querySnapshot = await getDocs(collection(db, "users"));
+        querySnapshot.forEach(child => {
+            usr_list.push(child._document.data.value.mapValue.fields);
+        })
+        setUsers(usr_list);
+    })()
+    console.log(users);
+
     return (
         <div className="left-column">
             <Toolbar/>
-            <div className="chat-list">
-
-            </div>
+            {users.length > 0 && (
+                <div className="chat-list">
+                    {users.map((user, index) => {
+                        return (
+                            <div key={index}>
+                                <ChatItem item={user}/>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
         </div>
     );
 }
